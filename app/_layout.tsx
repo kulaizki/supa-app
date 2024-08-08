74bc4a9fda16660841146dfe33e2008b8dda0b25
kulaizki <kulaizke@gmail.com>
2024-08-08 17:34:20 +0800

    refactor: remove unused imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
